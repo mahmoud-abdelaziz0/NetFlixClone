b1e78ae0418ead5e734ee5f370fc6ecb40d84c94
mahmoud abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2025-06-12 17:39:42 +0300

    put the content of navbar as a structure

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
+import Navbar from '../../components/Navbar/Navbar';
 import './Home.css'
 
 const Home = () => {
   return (
     <>
       <div className='home'>
-
+        <Navbar/>
       </div>
     </>
   );
