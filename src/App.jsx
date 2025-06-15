33f312975eb1a89e480de2dc172f620779e47dd9
mahmoud abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2025-06-15 15:48:13 +0300

    install react router library and make a routing just include a two pages are Home and Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
 
 function App() {
 
   return (
     <>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </>
   )
 }
