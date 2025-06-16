899fd06397fa51cf2e2372201b29285f808a6fe2
mahmoud abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2025-06-16 14:55:44 +0300

    build login structure

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,28 @@
 import './Login.css'
 
+import logo from '../../assets/logo.png';
+
 const Login = () => {
   return (
     <>
       <div className='login'>
-
+        <img src={logo} alt="Netflix Logo" className='login-logo'/>
+        <div className="login-form">
+          <h1>Sign Up</h1>
+          <form action="">
+            <input type="text" placeholder='Your Name'/>
+            <input type="email" placeholder='Email'/>
+            <input type="password" placeholder='Password'/>
+            <button>Sign In</button>
+            <div className="form-help">
+              <div className="remember">
+                <input id='remember' type="checkbox"/>
+                <label htmlFor="remember">Remember Me</label>
+              </div>
+              <p>Need Help?</p>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
