9bf63bd036284d6bd8c674e2e6e25ddcc9c2d268
Mahmoud Abdelaziz <itiwork262@gmail.com>
2025-06-18 20:53:27 +0300

    Adding the player page contnet

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login/>}/>
+        <Route path='/player/:id' element={<Player/>}/>
       </Routes>
     </>
   )
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,12 +1,28 @@
-import './Player.css'
+import "./Player.css";
+
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+
 const Player = () => {
   return (
     <>
       <div className="player">
-
+        <img src={back_arrow_icon} alt="" />
+        <iframe
+          src="https://www.youtube.com/embed/hkHHwA-vEyQ"
+          frameborder="0"
+          allowFullScreen
+          width="90%"
+          height="90%"
+          title="Trailer"
+        ></iframe>
+        <div className="player-info">
+          <p>Published Date</p>
+          <p>Name</p>
+          <p>Type</p>
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default Player;
